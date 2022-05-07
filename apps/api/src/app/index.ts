abf37e1ab6c2e434e4fb6477d4adcb941393a85b
Diana Anka <diana.anka@l-one.de>
2022-05-07 16:48:45 +0300

    Add error handler to server

--- apps/api/src/app/index.ts
+++ apps/api/src/app/index.ts
@@ -3,6 +3,7 @@ import * as express from 'express';
 import * as bodyParser from 'body-parser';
 
 import apiRouter from './routes/v1';
+import logger from './infra/logger';
 
 const app = express();
 
@@ -10,5 +11,21 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/api/v1', apiRouter);
+
+app.use(
+  (
+    err: Error,
+    req: express.Request,
+    res: express.Response,
+    _next: express.NextFunction
+  ) => {
+    logger.error(err);
+    res.status(500).json({
+      error: {
+        message: 'Something wrong happened',
+      },
+    });
+  }
+);
 
 export default app;
