ff34cec964b6407dca4dbe4266b403340eb5f7d4
Diana Anka <diana.anka@l-one.de>
2022-05-07 16:28:10 +0300

    Add getDB functionality

--- apps/api/src/app/infra/db.ts
+++ apps/api/src/app/infra/db.ts
@@ -1,9 +1,14 @@
 import { MongoClient } from 'mongodb';
-import { MONGO_URL } from '../config';
+import { DB_NAME, MONGO_URL } from '../config';
 
 const client = new MongoClient(MONGO_URL);
 
 export async function getMongoDBClient() {
   await client.connect();
   return client;
+}
+
+export async function getDB() {
+  const client = await getMongoDBClient();
+  return client.db(DB_NAME);
 }
