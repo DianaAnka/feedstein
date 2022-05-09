2ab3dcaa3560664139bf1644eace70723060302f
Diana Anka <diana.anka@l-one.de>
2022-05-09 13:43:48 +0300

    Add query client provider

--- apps/feed-stein/src/app/providers.tsx
+++ apps/feed-stein/src/app/providers.tsx
@@ -1,11 +1,18 @@
 import React, { ReactNode } from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from './theme';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 export interface ProvidersProps {
   children: ReactNode;
 }
 
+const queryClient = new QueryClient();
+
 export const Providers: React.FC<ProvidersProps> = ({ children }) => {
-  return <ChakraProvider theme={theme}>{children}</ChakraProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>{children}</ChakraProvider>
+    </QueryClientProvider>
+  );
 };
