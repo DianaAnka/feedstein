0a6bd57bac4b9e71016d2a00ce6c60ab0a2c5294
Diana Anka <diana.anka@l-one.de>
2022-05-10 11:53:28 +0300

    Add activate amail DTO

--- libs/api-interfaces/src/dto/auth-dtos.ts
+++ libs/api-interfaces/src/dto/auth-dtos.ts
@@ -1,3 +1,7 @@
 import { InserUserDTO } from '../repositories/user-repository';
 
 export type RegisterUserDTO = Pick<InserUserDTO, 'email' | 'password'>;
+
+export type ActivateEmailDTO = {
+  token: string;
+};
