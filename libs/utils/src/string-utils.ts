7d8d919d12d45fdfbd4788fb52b1f2748dd799a5
Diana Anka <diana.anka@l-one.de>
2022-05-10 10:39:47 +0300

    Add get random string function

--- libs/utils/src/string-utils.ts
+++ libs/utils/src/string-utils.ts
@@ -1,7 +1,12 @@
 import * as bcrybt from 'bcryptjs';
+import * as uuid from 'uuid';
 
 export async function hashString(rawString: string): Promise<string> {
   const salt = await bcrybt.genSalt(12);
   const hash = await bcrybt.hash(rawString, salt);
   return hash;
+}
+
+export function getRandomString() {
+  return uuid.v4();
 }
