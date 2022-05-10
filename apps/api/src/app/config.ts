9d17393b966952e7021b0c313b2c50ec7f4ecf3c
Diana Anka <diana.anka@l-one.de>
2022-05-10 10:37:37 +0300

    Add SMTP config

--- apps/api/src/app/config.ts
+++ apps/api/src/app/config.ts
@@ -4,3 +4,15 @@ export const MONGO_URL =
 export const DB_NAME = process.env.DB_NAME || 'feedstein';
 
 export const PORT = process.env.PORT || 7000;
+
+export const SMTP_USER = process.env.SMTP_USER;
+
+export const SMTP_PASSWORD = process.env.SMTP_PASSWORD;
+
+export const SMTP_HOST = process.env.SMTP_HOST;
+
+export const SMTP_PORT = Number.parseInt(process.env.SMTP_PORT, 10) || 587;
+
+export const SMTP_FROM = process.env.SMTP_FROM;
+
+export const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:3000';
