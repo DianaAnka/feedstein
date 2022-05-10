e815e5b5552d97966d268942614a1eed3ec17511
Diana Anka <diana.anka@l-one.de>
2022-05-10 10:41:57 +0300

    Emit event on register

--- apps/api/src/app/services/user-service.ts
+++ apps/api/src/app/services/user-service.ts
@@ -3,8 +3,11 @@ import {
   IUserSchema,
   RegisterUserDTO,
 } from '@feedstein/api-interfaces';
-import { hashString } from '@feedstein/utils';
+import { getRandomString, hashString } from '@feedstein/utils';
 import UserRepository from '../repositories/user-repository';
+import * as moment from 'moment';
+import Events, { EventType } from '../events/pub-sub';
+import { RegisterUserEvent } from '../events/auth-events';
 
 export class UserService {
   async isEmailAlreadyUsed(email: string): Promise<boolean> {
@@ -13,12 +16,24 @@ export class UserService {
   }
 
   async registerUser(data: RegisterUserDTO): Promise<IUserSchema> {
+    const activationToken = getRandomString();
+
+    const activationTokenExpirseAt = moment().add(1, 'day').toDate();
+
     const userDTO: InserUserDTO = {
       email: data.email,
       password: await hashString(data.password),
+      active: false,
+      activationToken,
+      activationTokenExpirseAt,
     };
     const user = await UserRepository.insert(userDTO);
+
+    Events.emit(EventType.REGISTER, new RegisterUserEvent(user));
+
     delete user.password;
+    delete user.activationToken;
+    delete user.activationTokenExpirseAt;
     return user;
   }
 }
