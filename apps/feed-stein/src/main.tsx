484ba6ccd9058ee2f0e6fb7960b378244bcfcdd4
Diana Anka <diana.anka@l-one.de>
2022-05-15 10:58:10 +0300

    Fix render bug by removing react strict mod

--- apps/feed-stein/src/main.tsx
+++ apps/feed-stein/src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react';
 import * as ReactDOMClient from 'react-dom/client';
 
 import App from './app/app';
@@ -6,8 +5,4 @@ import App from './app/app';
 const root = ReactDOMClient.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+root.render(<App />);
