e37e10f48ac6c72663eed0adb7630d6e190b160f
DianaTessafold <106163833+DianaTessafold@users.noreply.github.com>
2023-11-26 10:35:56 +0400

    add auth validation

--- libs/validation/src/auth-validation.ts
+++ libs/validation/src/auth-validation.ts
@@ -8,3 +8,16 @@ export const registerUserSchema = Yup.object({
 export const activateEmailSchema = Yup.object({
   token: Yup.string().required(),
 }).required();
+
+export const loginUserSchema = Yup.object({
+  email: Yup.string().email(),
+  password: Yup.string(),
+}).required();
+
+export const forgetPasswordSchema = Yup.object({
+  email: Yup.string().required(),
+}).required();
+
+export const resetPasswordSchema = Yup.object({
+  password: Yup.string().min(6).max(30),
+}).required();
